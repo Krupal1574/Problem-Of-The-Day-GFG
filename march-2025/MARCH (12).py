b8d286d7fdeaf1b52f7d0d1310968290c732b394
Krupal Prajapati <krupalprajapati007@outlook.com>
2025-04-02 22:31:00 +0530

    done

--- march-2025/MARCH (12).py
+++ march-2025/MARCH (12).py
@@ -1,6 +1,5 @@
 class Solution:
-    def countWays(self, n):
-        a, b = 1, 1
-        for _ in range(n - 1):
-            a, b = b, a + b
-        return b
+    def minCostClimbingStairs(self, cost):
+        for i in range(2, len(cost)):
+            cost[i] += min(cost[i - 1], cost[i - 2])
+        return min(cost[-1], cost[-2])
