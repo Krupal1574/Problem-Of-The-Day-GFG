55ec86a6ec17e1edb91083d9f33b41d2f7dd45e9
Krupal Prajapati <krupalprajapati007@outlook.com>
2025-05-28 22:40:37 +0530

    DONE

---
+++ MAY 2025/DAY-28 Find rectangle with corners as 1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def ValidCorner(self, mat):
+        n, m = len(mat), len(mat[0])
+        for i in range(n):
+            for j in range(i + 1, n):
+                cnt = sum(mat[i][k] & mat[j][k] for k in range(m))
+                if cnt > 1:
+                    return True
+        return False
